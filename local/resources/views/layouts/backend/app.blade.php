8733613967036217403b12cae48fab55e40989de
jinesh subedi <jinesh1094@gmail.com>
2020-08-05 10:17:13 +0545

    second commit

--- local/resources/views/layouts/backend/app.blade.php
+++ local/resources/views/layouts/backend/app.blade.php
@@ -173,7 +173,11 @@
 			        </div>
 			        <div class="col-sm-6 clearfix">
 			            <div class="user-profile pull-right">
-			                <img class="avatar user-thumb" src="assets/images/author/avatar.png" alt="avatar">
+			            	@if(Auth::user()->image)
+			                <img class="avatar user-thumb" src="{{asset('images/'.Auth::user()->image)}}" alt="avatar">
+			                @else
+			                <img class="avatar user-thumb" src="{{asset('/backend/assets/images/author/author-img4.jpg')}}" alt="avatar">
+			                @endif
 			                <h4 class="user-name dropdown-toggle" data-toggle="dropdown">{{Auth::user()->name}}<i class="fa fa-angle-down"></i></h4>
 			                <div class="dropdown-menu">
 			                	<a class="dropdown-item" href="#">
